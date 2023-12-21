0ffa245e342f74efba68e5e4baa5dd4150bebfb8
janekia <84926731+janekia@users.noreply.github.com>
2023-12-21 09:37:01 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,52 @@
+import par as par
+
+par = 0, 9, 18
+
+holes = "1", "2", "3", "4", "5", "6"
+
+name = input("Welcome to GC mini golf.What is your name? ")
+
+holes = input (str((f"Hi {name}! Would you like to play 3 or 6 today?")))
+if holes == holes:
+  print (str(input("How many putts for hole 1 ?")))
+
+elif holes == holes:
+  par = 3
+  print (str(input("How many putts for hole 1 ?")))
+
+if holes == holes:
+       par = 3
+       print (str(input(f"How many putts for  2 ?")))
+if holes == holes:
+    par  = 3
+    print (str(input("How many putts for hole  3?")))
+
+
+if holes == holes:
+       par  = 3
+       print (str(input("How many putts for hole 4 ?")))
+
+
+if holes == holes:
+       par = 3
+       print (str(input("How many putts for hole 5 ?")))
+
+
+if holes == holes:
+       par = 3
+
+
+       print (str(input("How many putts for hole 6 ?")))
+
+       parsum =(par * 6)
+       x = (parsum)
+
+if par == 3:
+    print(f"Nice try, {name}... Your total score was: +{x}")
+    print(par)
+elif par == 9:
+     print(f"Great job, {name}! Your total score was: -{x}.")
+else:
+     par == 0
+     print(f"Good game, {name}. Your total score was: 0.")
+
